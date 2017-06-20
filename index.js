9a045dece1b95d0039b40cad16a6ab32cb2c5fe1
Lena Faure <lena.faure@gmail.com>
2017-06-20 13:20:48 +0200

    Change welcome message

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var app = new alexa.app( 'test-skill' );
 
 
 app.launch( function( request, response ) {
-    response.say( 'Welcome to your test skill' ).reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
+    response.say( 'Welcome to Digicare' ).reprompt( 'What can I do for you today?' ).shouldEndSession( false );
 } );
 
 
